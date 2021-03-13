22bf96b385c729b798bea3071494eebd8e833fcf
Chamupathi Mendis <chamupathi.mendis@thirdfort.com>
2021-03-13 18:41:05 +0530

    add back end

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require( 'express' );
+
+const app = express();
+const bodyParser = require( 'body-parser' );
+const cors = require( 'cors' );
+const mongoose = require( 'mongoose' );
+
+app.use( cors() );
+
+app.use( bodyParser.json() );
+
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.zlwqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect( uri, { useNewUrlParser : true } );
+const { connection } = mongoose;
+
+connection.once( 'open', () => {
+	console.log( 'MongoDB database connection established successfully' );
+} );
+
+
+// error handler
+app.use( ( error, req, res, next ) => { // eslint-disable-line no-unused-vars
+	if ( error.isServer ) {
+		// log server errors 5xx status codes
+		logger.error( error );
+		return res.status( 500 ).json( 'internal server error' );
+	}
+	
+	console.log( error );
+
+	return res.status( 500 ).json( 'internel server error' );
+} );
+
+app.listen( config.serverPort, () => {
+	console.log( `Server is running on Port: ${config.serverPort}` );
+} );
