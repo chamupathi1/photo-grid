7987808e9e7842b2f166222d77e6a6f1d1b7ca2e
Chamupathi Mendis <chamupathi.mendis@thirdfort.com>
2021-03-13 16:33:50 +0530

    add photo reducer

---
+++ src/redux/actionTypes.js
@@ -0,0 +1,4 @@
+export const _FAILED = '_FAILED';
+export const _SUCCESS = '_SUCCESS';
+
+export const FETCH_ALL_PHOTOS = 'FETCH_ALL_PHOTOS';
--- src/redux/photo.reducer.js
+++ src/redux/photo.reducer.js
@@ -1,16 +1,19 @@
-// import {
-// 	CHANGE_LOADING,
-// 	API_FETCH_COMPANY,
-// 	_SUCCESS,
-// } from '../actionTypes';
+import { FETCH_ALL_PHOTOS } from "./actionTypes";
 
 const intitState = {
+        busy : false
 };
 
 export default function photoReducer( state = intitState, action ) {
 
 	switch ( action.type ) {
 
+                case FETCH_ALL_PHOTOS : {
+                        return {
+                                ...state,
+                                busy : true
+                        }
+                }
 		
 		default: {
 			return state;
