a7bba61ad2c80b21744dbe29ff1b647726f30e56
Chamupathi Mendis <chamupathi.mendis@thirdfort.com>
2021-03-13 16:26:22 +0530

    header bar

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import { Nav } from 'react-bootstrap';
+import { routes } from '../routes';
+import { Link, useLocation } from 'react-router-dom';
+
+const classes = {
+	nav: {
+		color: '#eee',
+		textDecoration: 'none',
+                paddingLeft : '10px'
+	},
+        selected : {
+                color: '#fff',
+                fontWeight : 500,
+        }
+};
 
 const Header = () => {
-        return (
-                <div>
-                        Header
-                </div>
-        );
+	const location = useLocation();
+
+	return (
+		<div>
+			<Navbar bg="dark" variant="dark">
+				<Navbar.Brand >Photo Grid</Navbar.Brand>
+				<Nav className="mr-auto">
+					{navs.map((nav) => getLink(location, nav))}
+				</Nav>
+			</Navbar>
+		</div>
+	);
 };
 
 export default Header;
+
+const navs = [
+        {
+		route: routes.HOME,
+		text: 'Home',
+	},
+	{
+		route: routes.SELECT,
+		text: 'Choose Photos',
+	},
+];
+
+const getLink = (location, nav) => {
+        const selected = location.pathname === nav.route;
+
+        const style = selected ? {...classes.nav, ...classes.selected} : classes.nav
+
+	return (
+		<Nav.Item>
+			<Link to={nav.route} style={style}>
+				{nav.text}
+			</Link>
+		</Nav.Item>
+	);
+};
